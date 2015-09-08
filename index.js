da2cbd1150236a3dffa0eb1758f770d593fb3702
Mihai Tomescu <matomesc@gmail.com>
2015-09-08 16:38:46 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,250 @@
+var crypto = require('crypto');
+var path = require('path');
+var url = require('url');
+var request = require('request');
+var Cache = require('lru-cache');
+
+var DOMAIN = 'myshopify.com';
+var INVALID_CHAR_RE = /[^0-9a-zA-Z.-]/;
+
+/**
+ * @class ShopifyAuth
+ */
+var ShopifyAuth = {};
+
+/**
+ * @param {String} appSecret
+ * @param {Object} params
+ * @returns {boolean}
+ */
+ShopifyAuth.checkIntegrity = function (appSecret, params) {
+  var hmac = params.hmac;
+
+  var message = Object.keys(params).filter(function (key) {
+    return key !== 'hmac' && key !== 'signature';
+  }).sort().map(function (key) {
+    // have to replace in this order,
+    var escapedKey = key.replace('%', '%25').replace('&', '%26').replace('=', '%3D'),
+      escapedVal = params[key].replace('%', '%25').replace('&', '%26');
+    return escapedKey + '=' + escapedVal;
+  }).join('&');
+
+  // get message signature
+  var ourSignature = crypto.createHmac('sha256', appSecret).update(message).digest('hex');
+
+  return ourSignature === hmac;
+};
+
+ShopifyAuth.checkShopHostname = function (hostname) {
+  var hasInvalidChar = INVALID_CHAR_RE.test(hostname);
+  var hasRightDomain = hostname.substring(hostname.length - DOMAIN.length) === DOMAIN;
+  return !hasInvalidChar && hasRightDomain;
+};
+
+/**
+ * @method exchangeCodeForToken
+ * @param {Object}   options
+ * @param {String}   options.appKey
+ * @param {String}   options.appSecret
+ * @param {String}   options.code
+ * @param {String}   options.shopDomain
+ * @param {Function} cb
+ * @static
+ */
+ShopifyAuth.exchangeCodeForToken = function (options, cb) {
+  var opts = {
+    url: 'https://' + options.shopDomain + '/admin/oauth/access_token',
+    form: {
+      client_id: options.appKey,
+      client_secret: options.appSecret,
+      code: options.code
+    }
+  };
+
+  request.post(opts, function (err, res, body) {
+    if (err) return next(err);
+    if (typeof body === 'string') {
+      try {
+        body = JSON.parse(body);
+      } catch (e) {
+        return cb(e);
+      }
+    }
+    if (!body.access_token) {
+      return cb(new Error('no access token supplied by shopify'));
+    }
+    return cb(null, body.access_token);
+  });
+};
+
+/**
+ * Express middleware for authenticating with Shopify via OAuth.
+ *
+ * Supports dynamic shop names and subsequent verification of
+ * incoming requests.
+ *
+ * Usage:
+ *
+ * ```
+ * var ShopifyAuth = require('./lib/shopify/auth');
+ *
+ * var auth = ShopifyAuth.create({
+ *  appKey: 'your_app_key',
+ *  appSecret: 'your_app_secret',
+ *  scope: ['read_products'],
+ *  authUrl: '/auth/shopify',
+ *  authCallbackUrl: '/auth/shopify/callback',
+ *  shop: function (req, done) {
+ *    done(req.query.shop);
+ *  },
+ *  beforePermission: function (redirectUrl, done) {
+ *    done();
+ *  },
+ *  afterAuth: function (accessToken, profile, done) {
+ *    // req.shop now contains the authenticated shop
+ *  }
+ * });
+ *
+ * app.use('/auth/shopify/', shopifyAuth)
+ * app.use('/auth/shopify/callback')
+ * ```
+ *
+ * Reference:
+ * https://docs.shopify.com/api/authentication/oauth
+ *
+ * @method create
+ * @param {Object}          options
+ * @param {String}          options.appKey
+ * @param {String}          options.appSecret
+ * @param {String}          options.baseUrl             Base url of the server (eg. `https://localhost:8000`)
+ * @param {String}          options.authPath            Path that starts the auth process.
+ * @param {String}          options.authCallbackPath    Path that Shopify will redirect shop admins after
+ *                                                      granting permissions.
+ * @param {[String]}        options.scope               Application scope. Eg. `['read_products', 'write_products']`
+ * @param {Function}        options.shop                Called with `(req, done)` and used to dynamically get
+ *                                                      the shop's myshopify domain from a request. Call `done(err,
+ *                                                      shop)` to continue.
+ * @param {String}          options.authSuccessUrl      User is redirected here if authentication succeeds.
+ * @param {String}          options.authFailUrl         User is redirected here if authentication fails.
+ * @param {Function}        [options.onPermission]      Called with `(shop, redirectUrl, done)` before redirecting to
+ *                                                      Shopify to ask for permissions.
+ * @param {Function}        options.onAuth              Called with `(shop, accessToken, done)` once authentication is
+ *                                                      done, but before redirecting to `authSuccessUrl`.
+ * @param {Function}        [options.onError]           Called with `(err, req, res, next)`.
+ */
+ShopifyAuth.create = function (options) {
+  var self = this;
+
+  // LRU cache to store randomly generated `state` during authentication
+  var cache = Cache({
+    max: 5000,
+    maxAge: 1000 * 60 * 5 // 5 minutes
+  });
+
+  var onError = options.onError;
+  if (!onError) {
+    onError = function (err, req, res, next) {
+      // log the error
+      if (err.stack) {
+        console.error(err.stack);
+      } else {
+        console.error(err);
+      }
+      // redirect
+      res.redirect(options.authFailUrl);
+    }
+  }
+
+  var middleware = function (req, res, next) {
+    if (req.path !== options.authPath && req.path !== options.authCallbackPath) {
+      return next();
+    }
+
+    if (req.path === options.authPath) {
+      return middleware.shop(req, function (err, shop) {
+        if (err) {
+          return middleware.onError(err, req, res, next);
+        }
+
+        var nonce;
+        try {
+          nonce = crypto.randomBytes(12).toString('hex');
+        } catch (e) {
+          nonce = crypto.pseudoRandomBytes(12).toString('hex');
+        }
+        cache.set(shop, nonce);
+
+        var redirectUrl = url.format({
+          protocol: 'https',
+          host: shop,
+          pathname: '/admin/oauth/authorize',
+          query: {
+            client_id: options.appKey,
+            scope: options.scope.join(','),
+            redirect_uri: url.resolve(options.baseUrl, options.authCallbackPath),
+            state: nonce
+          }
+        });
+
+        if (!middleware.onPermission) {
+          // redirect to shopify to ask for permission
+          return res.redirect(redirectUrl);
+        }
+
+        // call onPermission handler and redirect
+        return middleware.onPermission(shop, redirectUrl, function () {
+          return res.redirect(redirectUrl);
+        });
+      });
+    } else {
+      var params = req.query;
+
+      if (!(params && params.code && params.hmac && params.timestamp && params.state && params.shop)) {
+        var paramErr = new Error('ShopifyAuth: missing required query parameters (got ' +
+                                 Object.keys(params).join(',') + ')');
+        return middleware.onError(paramErr, req, res, next);
+      }
+
+      if (cache.get(params.shop) !== params.state) {
+        var stateErr = new Error('ShopifyAuth: state not found in cache');
+        return middleware.onError(stateErr, req, res, next);
+      }
+
+      if (!ShopifyAuth.checkIntegrity(options.appSecret, params)) {
+        var integrityErr = new Error('ShopifyAuth: integrity error (signature mismatch)');
+        return middleware.onError(integrityErr, req, res, next);
+      }
+
+      if (!ShopifyAuth.checkShopHostname(params.shop)) {
+        var shopErr = new Error('ShopifyAuth: invalid shop hostname `' + params.shop + '`');
+        return middleware.onError(shopErr, req, res, next);
+      }
+
+      var exchangeOptions = {
+        appKey: options.appKey,
+        appSecret: options.appSecret,
+        code: params.code,
+        shopDomain: params.shop
+      };
+      self.exchangeCodeForToken(exchangeOptions, function (err, accessToken) {
+        if (err) {
+          return onError(shopErr, req, res, next);
+        }
+        return middleware.onAuth(params.shop, accessToken, function () {
+          return res.redirect(options.authSuccessUrl);
+        });
+      });
+    }
+  };
+
+  middleware.shop = options.shop;
+
+  // attach handlers to middleware fn
+  middleware.onError = onError;
+  middleware.onAuth = options.onAuth;
+  middleware.onPermission = options.onPermission;
+
+  return middleware;
+};
+
+module.exports = ShopifyAuth;
---
+++ tests/index.js
@@ -0,0 +1,171 @@
+var assert = require('assert');
+var cp = require('child_process');
+var os = require('os');
+
+var express = require('express');
+var request = require('request');
+var sinon = require('sinon');
+
+var ShopifyAuth = require('../index');
+var testOptions = require('./options.json');
+
+function openBrowser(url) {
+  var cmd = os.platform().indexOf('win') !== -1 ? 'start' : 'open';
+  cmd += ' ' + url;
+  return cp.exec(cmd);
+}
+
+function defaultOptions() {
+  return {
+    appKey: testOptions.appKey,
+    appSecret: testOptions.appSecret,
+    baseUrl: 'http://localhost:8000',
+    authPath: '/auth',
+    authCallbackPath: '/auth/callback',
+    authSuccessUrl: '/success',
+    authFailUrl: '/fail',
+    scope: ['read_products'],
+    shop: function (req, done) {
+      done(null, req.query.shop);
+    }
+  };
+}
+
+describe('ShopifyAuth.create() middleware', function () {
+  var app;
+  var server;
+
+  beforeEach(function (done) {
+    app = express();
+    app.use(function (req, res, next) {
+      res.set({
+        Connection: 'close' // disable persistent connection
+      });
+      return next();
+    });
+    server = app.listen(8000, function (err) {
+      done(err);
+    });
+  });
+
+  afterEach(function (done) {
+    server.close(function (err) {
+      done(err);
+    });
+  });
+
+  it('should redirect to `authFailUrl` when signature fails integrity check', function (done) {
+    // stub checkIntegrity function to force integrity to fail
+    var checkIntegrity = ShopifyAuth.checkIntegrity;
+    var checkIntegrityStub = sinon.stub(ShopifyAuth, 'checkIntegrity', function (appSecret, params) {
+      // remove state parameter
+      delete params.state;
+
+      // call original, which will return false
+      return checkIntegrity(appSecret, params);
+    });
+
+    var options = defaultOptions();
+
+    options.onPermission = sinon.stub();
+    options.onPermission.callsArg(2);
+
+    options.onAuth = sinon.stub();
+    options.onAuth.callsArg(2);
+
+    var auth = ShopifyAuth.create(options);
+    var onErrorSpy = sinon.spy(auth, 'onError');
+
+    app.use(auth);
+    app.get('/fail', function (req, res) {
+      res.send('Auth fail');
+
+      assert(options.onPermission.callCount === 1);
+      assert(onErrorSpy.callCount === 1);
+      assert(options.onAuth.callCount === 0);
+
+      // restore original ShopifyAuth.checkIntegrity
+      ShopifyAuth.checkIntegrity.restore();
+
+      setTimeout(done, 500)
+    });
+
+    openBrowser('http://localhost:8000/auth?shop=' + testOptions.shop);
+  });
+
+  it('should call `onPermission` and `onAuth` handlers when authentication is successful', function (done) {
+    var options = defaultOptions();
+
+    options.onPermission = sinon.stub();
+    options.onPermission.callsArg(2);
+
+    options.onAuth = sinon.stub();
+    options.onAuth.callsArg(2);
+
+    options.onError = sinon.stub();
+
+    var auth = ShopifyAuth.create(options);
+
+    app.use(auth);
+    app.get('/success', function (req, res) {
+      res.send('Auth success');
+
+      assert(options.onError.callCount === 0);
+
+      assert(options.onPermission.calledOnce);
+      assert(options.onPermission.args[0][0] === testOptions.shop);
+      assert(options.onPermission.args[0][1].indexOf('https://') === 0);
+      assert(typeof options.onPermission.args[0][2] === 'function');
+
+      assert(options.onAuth.calledOnce);
+      assert(options.onAuth.args[0][0] === testOptions.shop);
+      assert(typeof options.onAuth.args[0][1] === 'string');
+      assert(typeof options.onAuth.args[0][2] === 'function');
+
+      setTimeout(done, 500);
+    });
+
+    openBrowser('http://localhost:8000/auth?shop=' + testOptions.shop);
+  });
+
+  it('should return a valid access token when authentication is successful', function (done) {
+    var options = defaultOptions();
+    var data;
+
+    options.onAuth = function (shop, accessToken, done_) {
+      var opts = {
+        method: 'GET',
+        url: 'https://' + shop + '/admin/shop.json',
+        headers: {
+          'X-Shopify-Access-Token':  accessToken,
+          'Accept': 'application/json'
+        }
+      };
+      request(opts, function (err, res, body) {
+        if (err) {
+          return done(err);
+        }
+        try {
+          data = JSON.parse(body);
+        } catch (e) {
+          return done(e);
+        }
+        done_();
+      });
+    };
+
+    var auth = ShopifyAuth.create(options);
+    app.use(auth);
+    app.get('/success', function (req, res, next) {
+      res.json(data);
+
+      assert(data.shop.id);
+      assert(data.shop.myshopify_domain === testOptions.shop);
+
+      setTimeout(done, 500);
+    });
+
+    openBrowser('http://localhost:8000/auth?shop=' + testOptions.shop);
+  });
+
+});
